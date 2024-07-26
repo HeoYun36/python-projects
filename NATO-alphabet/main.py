2a1a991cb4ca41d1af42087cf1e7ed8fa8c9fc02
Yun Heo <kokona0306@gmail.com>
2024-07-26 22:53:28 +0900

    use exception handling skill

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -1,14 +1,17 @@
 import pandas
-# TODO 1. Create a dictionary in this format:
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 alpha_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter a word: ").upper()
-# word_list = []
-# for letter in user_input:
-#     for (alpha, word) in alpha_dict.items():
-#         if letter == alpha:
-#             word_list.append(word)
-word_list = [alpha_dict[letter] for letter in user_input]
-print(word_list)
+
+def generate_alphabet():
+    user_input = input("Enter a word: ").upper()
+    try:
+        word_list = [alpha_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_alphabet()
+    else:
+        print(word_list)
+
+
+generate_alphabet()
